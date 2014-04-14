bb7390bbb8d7c688b79bbc094ebf26303eb42ef6
Xing, Wang <zenja.wang@gmail.com>
2014-04-14 21:30:33 +0800

    add shallow test for SparseFileGenerator

---
+++ tests/unit/test_sparse_file_generator.py
@@ -0,0 +1,5 @@
+from file_generators.sparse_file_generator import SparseFileGenerator
+
+if __name__ == '__main__':
+    generator = SparseFileGenerator(directory="./", prefix="sparse-", suffix=".txt")
+    generator.make_file(size=10240, repeat_str="hello!")
