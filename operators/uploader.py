d92031904144d0a5f41aa816a6b3305579c48cce
Xing, Wang <zenja.wang@gmail.com>
2014-04-14 15:08:53 +0800

    add uploader

---
+++ operators/uploader.py
@@ -0,0 +1,20 @@
+from drivers import driver
+import ntpath
+
+
+class Uploader(object):
+    def __init__(self, server_driver):
+        if not issubclass(driver.Driver, driver.Driver):
+            raise TypeError('Driver should be a subclass of drivers.driver.Driver')
+        self.driver = server_driver
+
+    def upload(self, local_filename, remote_filename=None):
+        """Upload a file to a cloud
+
+        Args:
+            filename: the filename of the local file to be uploaded
+        """
+        if remote_filename is None:
+            remote_filename = ntpath.basename(local_filename)
+        self.driver.upload(local_filename=local_filename,
+                           remote_filename=remote_filename)
