4e1b935cb9e246625b028af241f176ce5f8050a8
Xing, Wang <zenja.wang@gmail.com>
2014-05-01 21:25:26 +0800

    use command map to avoid redundant code

--- benchcloud/benchcloud.py
+++ benchcloud/benchcloud.py
@@ -7,19 +7,23 @@ def handle_uploader(extra_args):
     upload_task_runner.main(prog='uploader', args=extra_args)
 
 
-def handle_help():
+def handle_help(extra_args):
     print 'Help: to be built...'
 
+
+command_map = {
+    'uploader': handle_uploader,
+    'help': handle_help
+}
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print 'Usage: python benchcloud.py COMMAND [extra args]'
         sys.exit(-1)
     command = sys.argv[1]
-    if command == 'uploader':
-        handle_uploader(sys.argv[2:])
-    elif command == 'help':
-        handle_help()
+    if command in command_map:
+        command_map[command](sys.argv[2:])
     else:
         print 'Command not supported: {}'.format(command)
         sys.exit(-1)
+
